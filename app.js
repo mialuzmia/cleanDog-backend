b1d6b166e4be33a5d3ef8dd51312a4270d2853cf
Mia Azevedo <mialuzazevedo@gmail.com>
2024-02-02 01:12:44 -0400

    feat: added connection to mongoDB

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express(); //initializes express app
+
+const dbURL = 'mongodb+srv://mialuz:IEZoflXlk1pFFd4Q@cluster0.37zpk8d.mongodb.net/cleanDog?retryWrites=true&w=majority'
+
+const connectToDb = async () => {
+  const port = 3000;
+  const hostname = 'localhost';
+  try {
+    const result = await mongoose.connect(dbURL);
+    console.log('\nconnected to db\n');
+    app.listen(port, hostname, () => console.log(`server running at http://${hostname}:${port}\n`));
+  } catch (err) {
+    console.log(err);
+  };
+};
+
+connectToDb();
+
+app.use(morgan('dev'));
+
